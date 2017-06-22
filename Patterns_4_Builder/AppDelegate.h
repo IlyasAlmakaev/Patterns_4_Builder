e6ded16209252f410ce4dfae685890568ba380bb
Ильяс <i.almakaev@gmail.com>
2017-06-22 08:12:17 +0700

    Initial Commit

---
+++ Patterns_4_Builder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_4_Builder
+//
+//  Created by Ильяс on 22.06.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Patterns_4_Builder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patterns_4_Builder
+//
+//  Created by Ильяс on 22.06.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
